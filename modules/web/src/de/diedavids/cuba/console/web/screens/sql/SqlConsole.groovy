7545715959f98d3039032df9b70e3605d4e61e27
Mario David <mariodavid@arcor.de>
2017-03-07 09:28:53 +0100

    added excel export to SQL console

--- modules/web/src/de/diedavids/cuba/console/web/screens/sql/SqlConsole.groovy
+++ modules/web/src/de/diedavids/cuba/console/web/screens/sql/SqlConsole.groovy
@@ -4,10 +4,13 @@ import com.haulmont.chile.core.model.MetaClass
 import com.haulmont.chile.core.model.MetaProperty
 import com.haulmont.cuba.core.global.GlobalConfig
 import com.haulmont.cuba.core.global.Metadata
+import com.haulmont.cuba.gui.app.core.entityinspector.EntityInspectorBrowse
 import com.haulmont.cuba.gui.components.*
+import com.haulmont.cuba.gui.components.actions.ExcelAction
 import com.haulmont.cuba.gui.data.DsBuilder
 import com.haulmont.cuba.gui.data.impl.ValueCollectionDatasourceImpl
 import com.haulmont.cuba.gui.export.ExportDisplay
+import com.haulmont.cuba.gui.theme.ThemeConstants
 import com.haulmont.cuba.gui.upload.FileUploadingAPI
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory
 import de.diedavids.cuba.console.service.SqlConsoleService
@@ -52,30 +55,16 @@ class SqlConsole extends AbstractWindow {
 
     protected Table resultTable;
 
-    @Override
-    void init(Map<String, Object> params) {
-//        consoleFileUploadBtn.addFileUploadSucceedListener(new FileUploadField.FileUploadSucceedListener() {
-//            @Override
-//            void fileUploadSucceed(FileUploadField.FileUploadSucceedEvent e) {
-//                File file = fileUploadingAPI.getFile(consoleFileUploadBtn.fileId)
-//
-//
-//                showOptionDialog("File upload successful", "File upload successful. Execute the content of the file?", Frame.MessageType.CONFIRMATION, [new DialogAction(DialogAction.Type.OK) {
-//
-//                    @Override
-//                    void actionPerform(Component component) {
-//                        runGroovyScript(file.text)
-//                    }
-//                }, new DialogAction(DialogAction.Type.CANCEL)])
-//            }
-//        })
-//        consoleFileUploadBtn.addFileUploadErrorListener(new UploadField.FileUploadErrorListener() {
-//            @Override
-//            void fileUploadError(UploadField.FileUploadErrorEvent e) {
-//                showNotification("File upload error", Frame.NotificationType.ERROR)
-//            }
-//        })
-    }
+
+    @Inject
+    protected ThemeConstants themeConstants;
+    
+    @Inject
+    ButtonsPanel resultButtonPanel
+
+
+    @Inject
+    protected Button excelButton;
 
 
     void runSqlConsole() {
@@ -83,6 +72,8 @@ class SqlConsole extends AbstractWindow {
         ValueCollectionDatasourceImpl sqlResultDs = createDatasource(result)
         createResultTable(sqlResultDs)
     }
+
+
 
     private ValueCollectionDatasourceImpl createDatasource(Map<String, Object> result) {
         ValueCollectionDatasourceImpl sqlResultDs = creteValueCollectionDs()
@@ -111,7 +102,12 @@ class SqlConsole extends AbstractWindow {
         resultTable.setDatasource(sqlResultDs)
         resultTable.setSizeFull()
         resultTableBox.add(resultTable)
+
+        excelButton.enabled = true
+        excelButton.setAction(new ExcelAction(resultTable));
     }
+
+
 
     void clearSqlConsole() {
 
