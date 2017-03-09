5cf5b5bddb55f849b103a46b6546a51946b50557
Mario David <mariodavid@arcor.de>
2017-03-09 16:04:52 +0100

    created test for SqlConsoleService & SqlSelectResult

--- modules/core/src/de/diedavids/cuba/console/sql/SqlConsoleServiceBean.groovy
+++ modules/core/src/de/diedavids/cuba/console/sql/SqlConsoleServiceBean.groovy
@@ -2,9 +2,11 @@ package de.diedavids.cuba.console.sql
 
 import com.haulmont.cuba.core.Persistence
 import groovy.sql.Sql
+import net.sf.jsqlparser.statement.Statements
 import org.springframework.stereotype.Service
 
 import javax.inject.Inject
+import javax.sql.DataSource
 import java.sql.SQLException
 
 @Service(SqlConsoleService.NAME)
@@ -23,14 +25,24 @@ public class SqlConsoleServiceBean implements SqlConsoleService {
     SqlSelectResult executeSql(String sqlString) {
 
         def sqlStatements = sqlConsoleParser.analyseSql(sqlString)
-        def sql = new Sql(persistence.dataSource)
+        def sql = createSqlConnection(persistence.dataSource)
 
-        try {
-            def rows = sql.rows(sqlStatements.statements[0].toString())
-            selectResultFactory.createFromRows(rows)
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if (statementsAvailable(sqlStatements)) {
+            try {
+                def rows = sql.rows(sqlStatements.statements[0].toString())
+                selectResultFactory.createFromRows(rows)
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
+    }
+
+    protected boolean statementsAvailable(Statements sqlStatements) {
+        sqlStatements && sqlStatements.statements
+    }
+
+    protected Sql createSqlConnection(DataSource dataSource) {
+        new Sql(dataSource)
     }
 
 }
