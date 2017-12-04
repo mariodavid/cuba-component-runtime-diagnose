00a839c3d153a89e397141f713a2be8b3b569855
Evgeniy Loginov <loginov@haulmont.com>
2017-12-04 17:00:41 +0400

    Convert JPQL to SQL and copy SQL to clipboard

--- modules/web/src/de/diedavids/cuba/runtimediagnose/web/screens/jpql/JpqlConsole.groovy
+++ modules/web/src/de/diedavids/cuba/runtimediagnose/web/screens/jpql/JpqlConsole.groovy
@@ -1,25 +1,89 @@
 package de.diedavids.cuba.runtimediagnose.web.screens.jpql
 
+import static com.haulmont.cuba.gui.WindowManager.OpenType.DIALOG
+import static com.haulmont.cuba.gui.components.Frame.NotificationType.ERROR
+import static com.haulmont.cuba.gui.components.Frame.NotificationType.WARNING
+
 import com.haulmont.bali.util.ParamsMap
+import com.haulmont.cuba.core.global.Messages
+import com.haulmont.cuba.core.global.RemoteException
 import com.haulmont.cuba.gui.components.AbstractWindow
-import com.haulmont.cuba.gui.components.Frame
+import com.haulmont.cuba.gui.components.Button
+import com.haulmont.cuba.gui.components.SourceCodeEditor
+import com.haulmont.cuba.gui.components.actions.BaseAction
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory
+import de.diedavids.cuba.runtimediagnose.db.DbDiagnoseService
 import de.diedavids.cuba.runtimediagnose.diagnose.DiagnoseType
+import de.diedavids.cuba.runtimediagnose.web.screens.console.ConsoleFrame
 
 import javax.inject.Inject
 
-class JpqlConsole  extends AbstractWindow {
+class JpqlConsole extends AbstractWindow {
 
     @Inject
-    protected Frame consoleFrame
+    DbDiagnoseService dbDiagnoseService
+    @Inject
+    ConsoleFrame consoleFrame
+    @Inject
+    Messages messages
+    @Inject
+    ComponentsFactory componentsFactory
 
     @Override
     void init(Map<String, Object> params) {
         super.init(params)
 
-        openFrame(
+        consoleFrame = openFrame(
                 consoleFrame,
                 'console-frame',
                 ParamsMap.of('diagnoseType', DiagnoseType.JPQL)
         )
+
+        addSqlQueryButton()
+    }
+
+    void addSqlQueryButton() {
+        Button showSqlBtn = componentsFactory.createComponent(Button)
+        showSqlBtn.with {
+            caption = getMessage('generateSql')
+            setAction(new BaseAction('showSqlDialog').withHandler { e -> showSqlDialog() })
+        }
+
+        consoleFrame.getComponent('consoleInstrumentalPanel').add(showSqlBtn)
+    }
+
+    void showSqlDialog() {
+        SourceCodeEditor codeEditor = consoleFrame.getComponent('console') as SourceCodeEditor
+
+        if (codeEditorIsEmpty(codeEditor)) {
+            showNotification(getMessage('noScriptDefined'), WARNING)
+            return
+        }
+
+        try {
+            String sqlQuery = dbDiagnoseService.getSqlQuery(codeEditor.value)
+
+            if (sqlQuery) {
+                String clearQuery = removeFirstAndLastSquareBrackets(sqlQuery)
+                openWindow('sqlCopyDialog', DIALOG, ParamsMap.of('sqlQuery', clearQuery))
+            }
+        }
+        catch (RemoteException e) {
+            def illegalArgumentException = e.causes.any {
+                cause -> cause.className == 'java.lang.IllegalArgumentException'
+            }
+            if (illegalArgumentException) {
+                showNotification(getMessage('wrongScript'), ERROR)
+            }
+        }
+    }
+
+    static String removeFirstAndLastSquareBrackets(String query) {
+        query.replaceAll('^\\[?', '')
+                .replaceAll(']$', '')
+    }
+
+    boolean codeEditorIsEmpty(SourceCodeEditor codeEditor) {
+        codeEditor && !codeEditor.value
     }
 }
---
+++ modules/web/test/de/diedavids/cuba/runtimediagnose/web/screens/jpql/JpqlConsoleSpec.groovy
@@ -0,0 +1,118 @@
+package de.diedavids.cuba.runtimediagnose.web.screens.jpql
+
+import com.haulmont.cuba.core.global.RemoteException
+import com.haulmont.cuba.gui.components.AbstractWindow
+import com.haulmont.cuba.gui.components.Frame
+import com.haulmont.cuba.gui.components.SourceCodeEditor
+import de.diedavids.cuba.runtimediagnose.db.DbDiagnoseService
+import de.diedavids.cuba.runtimediagnose.web.screens.console.ConsoleFrame
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.haulmont.cuba.gui.WindowManager.OpenType
+import static com.haulmont.cuba.gui.WindowManager.OpenType.DIALOG
+
+class JpqlConsoleSpec extends Specification {
+
+    ConsoleFrame predefinedConsoleFrame
+    SourceCodeEditor predefinedCodeEditor
+    DbDiagnoseService predefinedDbDiagnoseService
+
+
+    def setup() {
+        predefinedCodeEditor = Mock()
+        predefinedConsoleFrame = Mock() {
+            getComponent('console') >> predefinedCodeEditor
+        }
+        predefinedDbDiagnoseService = Mock()
+    }
+
+    @Unroll
+    def "showSqlDialog show notification if text area is empty"() {
+
+        given:
+        predefinedCodeEditor.getValue() >> testVal
+
+        int countShowNotificationExecution = 0
+
+        def localSut = new JpqlConsole() {
+            protected String getMessage(String key) { "" }
+
+            void showNotification(String caption, Frame.NotificationType type) { countShowNotificationExecution++ }
+        }
+
+        localSut.with {
+            dbDiagnoseService = this.predefinedDbDiagnoseService
+            consoleFrame = this.predefinedConsoleFrame
+        }
+
+        when:
+        localSut.showSqlDialog()
+
+        then:
+        countShowNotificationExecution == 1
+
+        where:
+        testVal | _
+        ""      | _
+        null    | _
+    }
+
+    def "showSqlDialog a show exception dialog if the script in text area is wrong"() {
+
+        given:
+        int countExceptionNotification = 0
+        String noJpql = 'this string is not JPQL'
+        predefinedCodeEditor.getValue() >> noJpql
+        predefinedDbDiagnoseService.getSqlQuery(noJpql) >> { s -> throw new RemoteException(new IllegalArgumentException()) }
+
+        def localSut = new JpqlConsole() {
+            protected String getMessage(String key) { "" }
+
+            void showNotification(String caption, Frame.NotificationType type) { countExceptionNotification++ }
+        }
+
+        localSut.with {
+            dbDiagnoseService = this.predefinedDbDiagnoseService
+            consoleFrame = this.predefinedConsoleFrame
+        }
+
+        when:
+        localSut.showSqlDialog()
+
+        then:
+        countExceptionNotification == 1
+
+    }
+
+    def "showSqlDialog a show sql dialog if the script is correct"() {
+
+        given:
+        int countOpenSqlDialog = 0
+        String jpql = 'select u from sec$User u'
+        String sql = 'SELECT * FROM SEC_USER'
+        predefinedCodeEditor.getValue() >> jpql
+        predefinedDbDiagnoseService.getSqlQuery(jpql) >> "[${sql}]"
+
+        def localSut = new JpqlConsole() {
+            AbstractWindow openWindow(String alias, OpenType type, Map<String, Object> params) {
+                if (alias == 'sqlCopyDialog' && type == DIALOG && params.get('sqlQuery') == sql) {
+                    countOpenSqlDialog++
+                }
+                return null
+            }
+        }
+
+        localSut.with {
+            dbDiagnoseService = this.predefinedDbDiagnoseService
+            consoleFrame = this.predefinedConsoleFrame
+        }
+
+        when:
+        localSut.showSqlDialog()
+
+        then:
+        countOpenSqlDialog == 1
+
+    }
+}
